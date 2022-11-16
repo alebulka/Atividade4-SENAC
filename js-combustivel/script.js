8530651c0ab3b92db7ce98accf66efebfc95ebee
alebulka <bulkalessandra@outlook.com>
2022-11-16 17:27:10 -0300

    atividade resolução

---
+++ js-combustivel/script.js
@@ -0,0 +1,7 @@
+var escolha = window.prompt("escolha o combustível (considere '1-gasolina/ 2-álcool/ 3-diesel'):");
+
+if (escolha = "1" || "2" || "3") {
+    window.confirm("Obrigada pela preferência")
+} else {
+    window.alert("Opção inválida")
+}
