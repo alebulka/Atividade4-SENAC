b4a43f18a53a766cdfcdcfb5053faf3f407e295e
alebulka <bulkalessandra@outlook.com>
2022-11-18 16:08:05 -0300

    form criado

---
+++ for/atividade1/script.js
@@ -0,0 +1,11 @@
+// MinhaPrimeiraFuncao("oi");
+
+// function  MinhaPrimeiraFuncao(msg){
+//     alert("A mensagem enviada foi: "+msg);
+// }
+
+// oi("tudobem")
+
+// function oi(fodase) {
+//     alert(`msg ${fodase}`)
+// }
