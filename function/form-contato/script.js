fc762ecb8be54d34a0e61f343200f55a4d13beec
alebulka <bulkalessandra@outlook.com>
2022-11-30 17:51:26 -0300

    commit de configurações do email

--- function/form-contato/script.js
+++ function/form-contato/script.js
@@ -1,11 +1,25 @@
 
 
 function validar(){
-    if (document.dados.nome.value == "Alessandra") {
-        window.alert("Bem vinda Alessandra")
+    if (document.dados.nome.value == " ") {
+        window.alert("Digite seu nome")
         document.dados.nome.focus() 
         // .focus() -> vai focar naquele elemento até ele ser realizado
-    } else {
-        window.alert('parece q não é vc que tá entrando')
+        return false
+    }
+    if (document.dados.nome.value.lenght <3) {
+        window.alert("Digite o nome com mais de 3 caracteres")
+        document.dados.nome.focus() 
+        return false
+    }
+    if (document.dados.email.value == " ") {
+        window.alert("Digite o seu e-mail")
+        document.dados.email.focus() 
+        return false
+    }
+    if (document.dados.email.value.indexOf("gmail")== -1 || document.dados.email.value.indexOf("outlook")== -1) {
+        window.alert("E-mail inválido")
+        document.dados.email.focus() 
+        return false
     }
 }
